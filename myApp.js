57bb431df18240e7ecc40236059ebd4f7d739836
Angieleen Delos Reyes <angieleendelosreyes@Angieleens-MacBook-Pro.local>
2022-06-10 21:36:11 +0800

    fix path error

--- myApp.js
+++ myApp.js
@@ -1,9 +1,17 @@
 let express = require('express');
 let app = express();
 
-console.log('Hello World');
+// console.log('Hello World');
 
+app.get('/', function(req, res) {
+    console.log('Hello Express');
+    res.send('Hello Express');
+  })
 
+// app.get('/')
+//     .get(function(req, res) {
+//         res.send('Hello Express');
+//     })
 
 
 

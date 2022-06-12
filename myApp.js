8a44f3ae69bb9450311ec83f581f0ed40caf7ee9
angieleen delosreyes <38581452+angieleendelosreyes@users.noreply.github.com>
2022-06-12 13:23:21 +0000

    added json return in json get

--- myApp.js
+++ myApp.js
@@ -4,7 +4,12 @@ let app = express();
 
 app.get('/', function(req, res) {
     console.log('Hello Express');
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/json');
+  })
+
+app.get('/json', function(req, res) {
+    console.log('Hello Express');
+    res.json({"message": "Hello json"});
   })
 
 app.use('/public', express.static(__dirname + '/public'))

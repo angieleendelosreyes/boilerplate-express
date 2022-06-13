c04ce0c43044df71f52dd5c87cfe3cc60bbb7e59
angieleen delosreyes <38581452+angieleendelosreyes@users.noreply.github.com>
2022-06-13 08:06:28 +0000

    added middleware logging

--- myApp.js
+++ myApp.js
@@ -3,16 +3,26 @@ let app = express();
 require('dotenv').config();
 var response = "Hello json";
 
-app.get('/', function(req, res) {
-    console.log('Hello Express');
-    res.sendFile(__dirname + '/json');
-  })
+app.use('/', 
+  //       function(req, res) {
+  //   console.log('Hello Express');
+  //   res.sendFile(__dirname + '/json');
+  // }, 
+        function(req, res, next) {
+  console.log("I'm a middleware...");
+          console.log(req.method, req.path, " - " 
+                      ,req.ip);
+          express.static(__dirname + '/json')
+  next();
+})
 
 app.get('/json', function(req, res) {
-    console.log('Hello Express ' + process.env.MESSAGE_STYLE);
+  // console.log(req.method," ", req.path, " - " 
+  //                     ,req.ip);
+  //   console.log('Hello Express ' + process.env.MESSAGE_STYLE);
 
     if(process.env.MESSAGE_STYLE.toLowerCase() === 'uppercase' || process.env.MESSAGE_STYLE.toLowerCase() === 'allcaps'){
-      console.log('1st if');
+      // console.log('1st if');
         response = response.toUpperCase();
        console.log('1st end if', response);
     }else{

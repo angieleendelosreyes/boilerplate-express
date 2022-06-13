be26101968924f53677d0e3e9f195a4fc9681ea1
angieleen delosreyes <38581452+angieleendelosreyes@users.noreply.github.com>
2022-06-13 13:42:47 +0000

    added now and middleware func

--- myApp.js
+++ myApp.js
@@ -12,9 +12,22 @@ app.use('/',
   console.log("I'm a middleware...");
           console.log(req.method, req.path, " - " 
                       ,req.ip);
-          express.static(__dirname + '/json')
-  next();
+          next();
+          
+          // express.static(__dirname + '/now')
+  
 })
+
+app.get('/now', function(req, res, next) {
+  console.log('now get');
+  req.time = new Date().toString();
+          console.log(req.time);
+  next();
+}, function(req, res, next) {
+  // console.log('req.time', req.time); 
+  res.json({"time": req.time});
+}
+       );
 
 app.get('/json', function(req, res) {
   // console.log(req.method," ", req.path, " - " 
